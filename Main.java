5cb071b7aa3176974fe0f20468cbaebaab10fdcc
IbnRasAl-Fakih <rasul.eletai@gmail.com>
2023-06-04 00:57:22 +0600

    added

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        WeightedGraph<String> graph = new WeightedGraph<>(true);
+        MyGraph<String> myGraph = new MyGraph<>(true);
+
+        graph.addEdge("Almaty", "Astana", 2.1);
+        graph.addEdge("Almaty", "Shymkent", 7.2);
+        graph.addEdge("Shymkent", "Astana", 3.9);
+        graph.addEdge("Astana", "Kostanay", 3.5);
+        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
+
+        myGraph.addEdge("Almaty", "Astana");
+        myGraph.addEdge("Almaty", "Shymkent");
+        myGraph.addEdge("Shymkent", "Astana");
+        myGraph.addEdge("Astana", "Kostanay");
+        myGraph.addEdge("Shymkent", "Kyzylorda");
+
+        Search<String> djk = new DijkstraSearch<>(graph, "Almaty");
+        outputPath(djk, "Kyzylorda");
+        System.out.println();
+        Search<String> dfs = new DepthFirstSearch<>(myGraph, "Almaty");
+        outputPath(dfs, "Kyzylorda");
+        System.out.println();
+        Search<String> bfs = new BreadthFirstSearch<>(myGraph, "Almaty");
+        outputPath(bfs, "Kyzylorda");
+    }
+
+    public static void outputPath(Search<String> search, String key) {
+        for (String v : search.pathTo(key)) {
+            System.out.print(v + " -> ");
+        }
+    }
+}
